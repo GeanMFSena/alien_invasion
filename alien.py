7b5adf8d6d712644bcd2ef9aa4e1017e007a32fc
Gean Marcos Figueredo Sena <122699523+GeanMFSena@users.noreply.github.com>
2025-12-04 22:12:58 -0300

    colocando os comentarios

--- alien.py
+++ alien.py
@@ -12,14 +12,18 @@ class Alien(Sprite):
         self.settings = ai_game.settings 
         self.screen_rect = ai_game.screen.get_rect() 
         
+        
+        # pega a imagem e o rect dela
         self.image = pygame.image.load('alien.bmp')
         self.rect = self.image.get_rect()
         
         # self.rect.topleft = self.screen_rect.topleft 
         
+        # adiciona o alien na parte superior da tela um pouco afastado 
         self.rect.x = self.screen.width
         self.rect.y = self.screen.height
         
+        # armazena a posicao horizontal do alien 
         self.x = float(self.rect.x)
         
         
