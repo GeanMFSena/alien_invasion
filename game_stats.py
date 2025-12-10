f966138f2fc7ba432964a80f12bd9656008d6efa
Gean Marcos Figueredo Sena <122699523+GeanMFSena@users.noreply.github.com>
2025-12-09 21:21:10 -0300

    criando a classe que contem as estatisticas do jogo e a contagem de mortes

---
+++ game_stats.py
@@ -0,0 +1,12 @@
+import sys 
+
+
+class GameStats:
+    '''Rastreia as estatisticas da invasao alienigena '''
+    
+    def __init__(self,ai_game):
+        self.settings = ai_game.settings
+        self.reset_stats()
+        
+    def reset_stats(self):
+        self.ship_left = self.settings.ship_limit
